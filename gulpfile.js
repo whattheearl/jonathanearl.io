aa9ff029e7ac070218e8e5d25843047813002a52
jonathan earl <earl.jonathan@gmail.com>
2018-07-30 22:49:01 -0700

    initial

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+let gulp = require('gulp')
+let browserSync = require('browser-sync').create();
+let sass = require('gulp-sass')
+
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        server:{
+            baseDir: './'
+        }
+    })
+})
+
+
+gulp.task('sass', () => {
+    return gulp.src('src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'))
+})
+
+gulp.task('default', ['browser-sync'], (done) => {
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+    gulp.watch('*.html').on('change', browserSync.reload)
+    gulp.watch('css/*.css').on('change', browserSync.reload)
+})
