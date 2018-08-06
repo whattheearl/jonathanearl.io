d5c8ce9b7c849ddc6060e6ddfb58d66183fa043d
jonathan earl <earl.jonathan@gmail.com>
2018-08-06 13:28:10 -0700

    switch from gulp to webpack

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// Show hide mobile nav
+import '../style/index.scss'
+import queryString from 'query-string';
+
+let navButton = document.querySelector('.nav-bar__menu-button')
+navButton.onclick = e => {
+    e.preventDefault()
+    document.querySelector('.nav-bar__list').classList.toggle('hidden')
+}
+
+const parsed = queryString.parse(window.location.search)
+
+let notification = document.querySelector('.contact__notification')
+console.log('notification', notification)
+if(parsed.success) console.log('success')
+switch(parsed.success) {
+    case undefined: 
+        break;
+    case 'true': 
+        notification.innerHTML = 'Talk to you soon!';
+        break;
+    case 'false':
+        notification.innerHTML = 'Error, please try again in a little bit';
+        break;
+}
+
+console.log('oh man friday the 13th')
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+        ]
+    }
+}
