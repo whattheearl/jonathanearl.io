5417e59843f59d21ae57eb4ccb8f1163df4506ef
jonathan earl <earl.jonathan@gmail.com>
2018-08-09 13:22:32 -0700

    fix nav open and close, make internal anchor ref go to top of document area

--- src/js/components/contact-notification.js
+++ src/js/components/contact-notification.js
@@ -5,11 +5,20 @@ let notification = document.querySelector('.nav-bar__notification')
 
 switch(parsed.success) {
     case undefined: 
+        notification.innerHTML = null;
         break;
     case 'true': 
-        notification.innerHTML = 'Talk to you soon!';
+        notification.innerHTML = 'Talk to you soon!'
+        notification.style.display = 'block'
         break;
     case 'false':
-        notification.innerHTML = 'Error, please try again in a moment';
+        notification.innerHTML = 'Error, please try again in a moment'
+        notification.style.display = 'block'
         break;
+}
+
+notification.onclick = (e) => {
+    e.preventDefault()
+
+    notification.style.display = 'none'
 }
--- src/js/components/nav-button.js
+++ src/js/components/nav-button.js
@@ -1,5 +1,35 @@
 let navButton = document.querySelector('.nav-bar__menu-button')
 navButton.onclick = e => {
     e.preventDefault()
-    document.querySelector('.nav-bar__list').classList.toggle('hidden')
+    e.stopPropagation()
+    let navBarList = document.querySelector('.nav-bar__list')
+    navBarList.classList.toggle('hidden')
+    console.log('button', navBarList)
+}
+
+let navBarList = document.querySelector('.nav-bar__list')
+navBarList.onclick = e => {
+    e.stopPropagation();
+    console.log('navBarList')
+}
+
+let navBarItem = document.querySelectorAll('.nav-bar__item > a')
+navBarItem.forEach(item => {
+    item.onclick = e => {
+        e.stopPropagation()
+        let navBarList = document.querySelector('.nav-bar__list')
+        navBarList.classList.add('hidden')
+        console.log('navBarItem', navBarList)
+    }
+})
+    
+
+
+
+document.body.onclick = e => {
+    let navBarList = document.querySelector('.nav-bar__list')
+    if(!navBarList.classList.contains('hidden')) {
+        navBarList.classList.add('hidden')
+    }
+    console.log('body', navBarList)
 }
