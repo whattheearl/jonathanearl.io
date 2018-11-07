e93c62c21566efaf6fb7f42311f666025c3775cd
jonathan earl <earl.jonathan@gmail.com>
2018-11-07 15:22:32 -0800

    add documentation and clean up style/js files

--- src/js/contact-notification.js
+++ src/js/contact-notification.js
@@ -1,24 +1,36 @@
-import axios from 'axios'
+/*
+    Handle contact-form submission
+    Handle notification-bar msg and hide
+*/
+import axios from 'axios'       // Ajax calls
 
+// Returns Value of input selector
 let getInputValue = selector => {
     return document.querySelector(selector).value
 }
 
+// Clears Value of input selector
 let clearInputValue = (selector) => {
     document.querySelector(selector).value = ""
 }
 
+// Handle submission event
+// Submits to cloud function which sends an email to earl.jonathan@gmail.com and to the user
 let form = document.querySelector('form')
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     let notification = document.querySelector('.nav-bar__notification')
 
+    // Get form data
     let formData = {
         name: getInputValue('.contact__name'),
         email: getInputValue('.contact__email'),
         msg: getInputValue('.contact__msg')
     }
 
+    // Submit form data
+    // Success: clear data display success message
+    // Failure: display error message
     axios.post("https://us-central1-jonathanearlio.cloudfunctions.net/contact-service/", formData)
     .then(res => {
         notification.innerHTML = res.data.msg
@@ -39,21 +51,9 @@ form.addEventListener('submit', (e) => {
     })
 })
 
-// switch(parsed.success) {
-//     case undefined: 
-//         notification.innerHTML = null;
-//         break;
-//     case 'true': 
-//         notification.style.display = 'block'
-//         break;
-//     case 'false':
-//         notification.innerHTML = 'Error, please try again in a moment'
-//         notification.style.display = 'block'
-//         break;
-// }
+// Allow user to hide notification bar by clicking it
 let notification = document.querySelector('.nav-bar__notification')
 notification.onclick = (e) => {
     e.preventDefault()
-
     notification.style.display = 'none'
 }
--- src/js/nav-button.js
+++ src/js/nav-button.js
@@ -1,35 +1,36 @@
+/*
+    Handle mobile navigation menu events
+*/
+
+// display mobile nav
 let navButton = document.querySelector('.nav-bar__menu-button')
 navButton.onclick = e => {
     e.preventDefault()
     e.stopPropagation()
     let navBarList = document.querySelector('.nav-bar__list')
     navBarList.classList.toggle('hidden')
-    console.log('button', navBarList)
 }
 
+// stop click through
 let navBarList = document.querySelector('.nav-bar__list')
 navBarList.onclick = e => {
     e.stopPropagation();
-    console.log('navBarList')
 }
 
+// hide navigation after selection
 let navBarItem = document.querySelectorAll('.nav-bar__item > a')
 navBarItem.forEach(item => {
     item.onclick = e => {
         e.stopPropagation()
         let navBarList = document.querySelector('.nav-bar__list')
         navBarList.classList.add('hidden')
-        console.log('navBarItem', navBarList)
     }
 })
     
-
-
-
+// hide menu if clicked off the menu
 document.body.onclick = e => {
     let navBarList = document.querySelector('.nav-bar__list')
     if(!navBarList.classList.contains('hidden')) {
         navBarList.classList.add('hidden')
     }
-    console.log('body', navBarList)
 }
