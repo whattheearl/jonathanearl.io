274eb0e5de65ac0a96f4fd7d4be17ce8dc42e751
whattheearl <earl.jonathan@gmail.com>
2021-02-15 23:43:33 -0800

    fix: enable cors

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,9 +1,11 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: process.env.ADMIN_APP_ORIGIN } as CorsOptions);
 
   const config = new DocumentBuilder()
     .setTitle('ProfileService')
