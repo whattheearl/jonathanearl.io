3852c126f7bcbb16889951f3264947746e25c2ed
whattheearl <earl.jonathan@gmail.com>
2021-03-07 14:46:21 -0800

    ci: specify site deployment

--- jonathanearl.io/src/placeholder/index.tsx
+++ jonathanearl.io/src/placeholder/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Placeholder() {
     return (
         <div>
-            <h1>Jonathans (soon to be) awesome website!</h1>
+            <h1>Jonathans (soon to be) awesome website!!!</h1>
             <div>This site is mostly for my engineering experiments.</div>
         </div>
     )
