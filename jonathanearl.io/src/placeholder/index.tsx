b559f943d67ad68be6317e692fa56b6fd486e84e
whattheearl <earl.jonathan@gmail.com>
2021-03-07 15:01:06 -0800

    Revert "ci: turn off caching"
    This reverts commit 25a37c43b27b408650be7a5998e2aadae34df918.

--- jonathanearl.io/src/placeholder/index.tsx
+++ jonathanearl.io/src/placeholder/index.tsx
@@ -5,7 +5,6 @@ export default function Placeholder() {
         <div>
             <h1>Jonathans (soon to be) awesome website!!!</h1>
             <div>This site is mostly for my engineering experiments.</div>
-            <div>Test ttl changes</div>
         </div>
     )
 }
